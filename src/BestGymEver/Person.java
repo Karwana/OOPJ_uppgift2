a84014b1e9d2c75d461bd70c0e4f6d656d952a3f
Karwan Ali <karwana0000@gmail.com>
2025-10-17 16:04:40 +0200

    Person variabler, konstruktor

---
+++ src/BestGymEver/Person.java
@@ -0,0 +1,18 @@
+package BestGymEver;
+
+public class Person {
+    private String namn;
+    private String adress;
+    private String mailadress;
+    private int personnummer;
+
+    Person(){}
+    Person(String namn, String adress, String mailadress, int personnummer) {
+        this.namn = namn;
+        this.adress = adress;
+        this.mailadress = mailadress;
+        this.personnummer = personnummer;
+    }
+
+
+}
