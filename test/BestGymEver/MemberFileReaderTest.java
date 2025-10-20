e262b2a822c1630b621cf3854939d74c15abe3f7
Karwan Ali <karwana0000@gmail.com>
2025-10-20 20:49:45 +0200

    tester created for MemberFileReader

---
+++ test/BestGymEver/MemberFileReaderTest.java
@@ -0,0 +1,13 @@
+package BestGymEver;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MemberFileReaderTest {
+
+    MemberFileReader r = new MemberFileReader();
+
+    Path p = Paths.get("test/BestGymEver/gym_medlemmar.txt");
+
+
+}
