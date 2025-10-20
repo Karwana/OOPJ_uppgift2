702263949c093c779f5d7228abbdbdec828c2220
Karwan Ali <karwana0000@gmail.com>
2025-10-20 15:49:59 +0200

    Test for Person class

---
+++ test/BestGymEver/MemberTest.java
@@ -0,0 +1,15 @@
+package BestGymEver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberTest {
+
+    Member m = new Member();
+
+    @Test
+
+
+
+}
--- test/BestGymEver/PersonTest.java
+++ test/BestGymEver/PersonTest.java
@@ -1,8 +1,25 @@
 package BestGymEver;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class PersonTest {
+
+    Person p = new Person("Fredrik Berggren", "Skolgränd 8, 16819 Norrköping", "fredde@fakemail.se", "851020-6728");
+
+   @Test
+    void testPersonConstructorAndGetter() {
+
+       assertEquals("Fredrik Berggren", p.getNamn());
+       assertEquals("Skolgränd 8, 16819 Norrköping", p.getAdress());
+       assertEquals("fredde@fakemail.se", p.getMailadress());
+       assertEquals("851020-6728", p.getPersonnummer());
+       assertNotEquals("189271-3718", p.getPersonnummer());
+
+
+
+   }
 
 
 
