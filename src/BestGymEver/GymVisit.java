48df9ecae56c386e1f4b3ef42d8c1c406ba0a548
Karwan Ali <karwana0000@gmail.com>
2025-10-21 21:41:04 +0200

    GymVisitWriter updated, added FileWriter

--- src/BestGymEver/GymVisit.java
+++ src/BestGymEver/GymVisit.java
@@ -1,16 +1,22 @@
 package BestGymEver;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class GymVisit {
     private String memberName;
     private String personnummer;
     private LocalDate visitDate;
+    private LocalTime visitTime;
 
     public GymVisit(String memberName, String personnummer, LocalDate visitDate) {
         this.memberName = memberName;
         this.personnummer = personnummer;
         this.visitDate = visitDate;
+    }
+
+    public LocalTime getVisitTime() {
+        return visitTime;
     }
 
     public String getMemberName() {
--- src/BestGymEver/GymVisitWriter.java
+++ src/BestGymEver/GymVisitWriter.java
@@ -1,7 +1,25 @@
 package BestGymEver;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class GymVisitWriter {
 
     private String filePath = "src/BestGymEver/gym_visits.txt";
 
+    public void writeVisit(GymVisit visitor, String platina) {
+
+        String line = visitor.getMemberName() + " (" + visitor.getPersonnummer() + ") - " +
+                visitor.getVisitDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + " " +
+                visitor.getVisitTime().format(DateTimeFormatter.ofPattern("HH:mm"));
+
+        try (FileWriter writer = new FileWriter(filePath, true)) {
+            writer.write(line + "\n");
+
+        } catch (IOException e) {
+            System.err.println("Kunde inte skriva till fil: " + e.getMessage());
+        }
+    }
 }
