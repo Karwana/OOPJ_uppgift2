d1f51c121c4ad9983b00f4d70ba54deb6fdf88e4
Karwan Ali <karwana0000@gmail.com>
2025-10-21 21:24:12 +0200

    test created

--- test/BestGymEver/GymVisitWriterTest.java
+++ test/BestGymEver/GymVisitWriterTest.java
@@ -1,6 +1,21 @@
 package BestGymEver;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class GymVisitWriterTest {
 
-    // testa att när member gör gymbesök -> skriver rätt värde till fil
+    GymVisitWriter writer = new GymVisitWriter();
+
+    @Test
+    void writeVisitTest() {
+        GymVisit visitor = new GymVisit("Fredrik Berggren", "851020-6728", LocalDate.of(2025, 10, 21));
+        writer.writeVisit(visitor, "Platina");
+        assertTrue(true);
+
+    }
+
 }
