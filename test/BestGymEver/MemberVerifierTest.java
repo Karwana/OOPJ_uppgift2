0fd5ba1c22dcd5fb3b042049b49c53e0c6b6ee50
Karwan Ali <karwana0000@gmail.com>
2025-10-21 20:05:38 +0200

    cleaned up MemberVerifierTest added test for non member

--- test/BestGymEver/MemberVerifierTest.java
+++ test/BestGymEver/MemberVerifierTest.java
@@ -5,42 +5,29 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MemberVerifierTest {
 
-    MemberVerifier v = new MemberVerifier();
+    @Test
+    void findMemberExists() {
+        Member fredrik = new Member("Fredrik Berggren", "Skolgränd 8", "fredde@fakemail.se",
+                "851020-6728", LocalDate.of(2023,10,20), LocalDate.now().minusDays(20), "Platina");
+
+        MemberVerifier verifier = new MemberVerifier(List.of(fredrik));
+
+        // Testar att medlem med personnummer finns
+        assertEquals(fredrik, verifier.findMember("851020-6728"));
+    }
 
     @Test
-    void findExistingMemberTest() {
-        Member payingMember = createDummyPayingMember();
-        List<Member> list = List.of(payingMember);
+    void findMemberNotExists() {
+        Member fredrik = new Member("Fredrik Berggren", "Skolgränd 8", "fredde@fakemail.se",
+                "851020-6728", LocalDate.of(2023,10,20), LocalDate.now().minusDays(20), "Platina");
 
-        Member find = v.findMember("881231-1335");
+        MemberVerifier verifier = new MemberVerifier(List.of(fredrik));
 
-        assertEquals(payingMember, find);
+        // Testar att medlem med okänt personnummer inte finns
+        assertNull(verifier.findMember("000000-0000"));
     }
-
-    private Member createDummyPayingMember() {
-        return new Member("Fredrik Berggren",
-                "Kungsgatan 8",
-                "fredrik.berggren@hotmail.com",
-                "881231-1335",
-                LocalDate.of(2019,12,12),
-                LocalDate.now().minusDays(20),
-                "Guld");
-    }
-
-    // testa verify metod
-
-    // testa find metod när medlem finns i listan
-    // testa find metod när medlem inte finns i listan
-    /*
-
-
-
-    test: kolla om fredrik finns -> ja
-    test: kolla att jens INTE finns -> ja
-     */
-
 }
