927d871388b05ebe4a6c3320ed8df047af7d8dc2
Karwan Ali <karwana0000@gmail.com>
2025-10-21 15:39:33 +0200

    cleanup + fileNotFoundTest

--- test/BestGymEver/MemberFileReaderTest.java
+++ test/BestGymEver/MemberFileReaderTest.java
@@ -1,7 +1,6 @@
 package BestGymEver;
 
 import org.junit.jupiter.api.Test;
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -12,25 +11,39 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MemberFileReaderTest {
 
     MemberFileReader r = new MemberFileReader();
-
     Path p = Paths.get("test/BestGymEver/gym_medlemmar.txt");
 
     @Test
-    public void readListTest() {
+    void readMembersTest() {
         List<Member> l = r.read(p);
 
+        assertNotNull(l);
+        assertFalse(l.isEmpty());
         assertEquals(20, l.size());
 
-        // Kontrollera första medlem
+
+
+        // Testa första medlem
         Member first = l.getFirst();
         assertEquals("Fredrik Berggren", first.getNamn());
-        assertEquals("Platina", first.getMemberType());
         assertEquals("851020-6728", first.getPersonnummer());
-
-        // Kontrollera att nästa medlem INTE är Maria
-        Member second = l.get(1);
-        assertNotEquals("Maria Holmberg", second.getNamn());
+        assertEquals("Platina", first.getMemberType());
+        assertEquals("Skolgränd 8, 16819 Norrköping", first.getAdress());
+        assertEquals("fredde@fakemail.se", first.getMailadress());
     }
 
+    @Test
+    void readDatesTest() {
+        List<Member> l = r.read(p);
 
+        Member first = l.getFirst();
+        assertEquals(LocalDate.of(2021, 12, 30), first.getMemberLatestUpdateDate());
+    }
+
+    @Test
+    void fileNotFoundTest() {
+        Path invalid = Paths.get("test/BestGymEver/does_not_exist.txt");
+        List<Member> memberList = r.read(invalid);
+        assertNull(memberList);
+    }
 }
