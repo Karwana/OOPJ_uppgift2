cf369167d15e3013bfb6abfd117d2f30953a3e21
Karwan Ali <karwana0000@gmail.com>
2025-10-21 19:56:13 +0200

    MemberVerifier added method

--- src/BestGymEver/MemberVerifier.java
+++ src/BestGymEver/MemberVerifier.java
@@ -1,13 +1,39 @@
 package BestGymEver;
 
+import java.util.List;
+
 public class MemberVerifier {
 
+    private List<Member> members;
+
+    public MemberVerifier(List<Member> members) {
+        this.members = members;
+    }
+
+    // Sök personnummer
     public Member findMember(String personNummer) {
-        return null;
+        if (personNummer == null) return null;
+
+        for (Member member : members) {
+            if (personNummer.equals(member.getPersonnummer())) {
+                return member;
+            }
+        }
+        return null; // medlem finns inte
     }
 
+    // Sök namn
     public Member findMember(String firstName, String lastName) {
+        if (firstName == null || lastName == null) return null;
 
-        return ;
+        String fullName = firstName + " " + lastName;
+        for (Member member : members) { // <-- här var felet
+            if (fullName.equalsIgnoreCase(member.getNamn())) {
+                return member;
+            }
+        }
+        return null; // medlem finns inte
     }
+
+
 }
